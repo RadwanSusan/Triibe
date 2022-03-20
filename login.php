3eede7c8d72b63131ef9df18993ec9cfb840c522
ZaidAltamari <93926834+ZaidAltamari@users.noreply.github.com>
2022-03-20 21:25:32 +0200

    php2

--- login.php
+++ login.php
@@ -35,7 +35,7 @@ session_start();
       if($count == 1) {
          //session_register("myusername");
          $_SESSION['login_user'] = $myusername;
-         echo "looged in";
+         echo "<p>looged in</p>";
         // header("location: homepage.php");
       }else {
          echo"Your Login Name or Password is invalid";
@@ -46,58 +46,39 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
-    <link rel="stylesheet" href="bootstrap-css/bootstrap.min.css">
-    <link rel="stylesheet" href="bootstrap-css/all.min.css">
-    <link rel="stylesheet" href="bootstrap-css/login-style.css">
-    <script src="bootstrap-js/login-js"></script>
-</head>
-<body>
-
-    <div class="container">
-        <div class="row content">
-          <div class="col-lg-6 col-md-12 ">
-          <div class="box">
-            <div class="inside-box">
-                <img class="img1" src="Design/Image/Untitled-1.svg" alt="image">
-                <div class="Title">
-                  Triibe
-                </div>
-            </div>
-            <p class="p1">Connect With Your Friends Inside The University On Triibe</p>
-            
-                </div>
-          </div>
-          <div class="col-lg-6">
-            <div class="card">
-              
-            <form class="form-login" action="" method="POST" >
-                <div class="mb-3">
-                  <input type="text" name="uname" class="form-control e-1" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email or Phone Number">
-                </div>
-                <div class="mb-3">
-                  <input type="password" name="password" class="form-control e-1" id="exampleInputPassword1" placeholder="Password">
-                </div>
-                <button type="submit" class="btn btn-primary form-control e-1 e-2">Log In</button>
-                <a class="link-forgot" href="">Forgot password?</a>
-                <button type="submit" class="btn btn-primary form-control e-1 e-3"> Create new Account</button>
-
-            </form>   
-            
-              
-              </div>
-              </div>
-          </div>
+    <link rel="stylesheet" href="bootstrap-css/bootstrap.min.css" />
+    <link rel="stylesheet" href="bootstrap-css/all.min.css" />
+    <link rel="stylesheet" href="bootstrap-css/login-style.css" />
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="bootstrap-js/login.js"></script>
+  </head>
+  <body>
+    <div class="container1">
+      <div class="left">
+        <div class="box">
+          <img src="Design/Image/Untitled-1.svg" alt="Triibe" />
+          <h1>Triibe</h1>
         </div>
-      </div>  
-
-
-        <script src="bootstrap-js/bootstrap.bundle.min.js"></script>
-        <script src="bootstrap-js/all.min.js"></script>
-        <script src="/bootstrap-js/jquery/dist/jquery.min.js"></script>
-        </body>
+        <p>Connect With Your Friends Inside The University On Triibe .</p>
+      </div>
+      <div class="right">
+        <form action="" method="POST">
+          <input type="text" name="uname" placeholder="Email Or student number" />
+          <input type="password" name="password" placeholder="Password" />
+          <button type="submit" href="" class="loginBtn">Log In</button>
+          <a href="" class="forget">Forgotten password?</a>
+          <div class="sign-up">
+            <a href="" class="sign-up-btn">Create New Account</a>
+          </div>
+        </form>
+      </div>
+    </div>
+    <script src="bootstrap-js/bootstrap.bundle.min.js"></script>
+    <script src="bootstrap-js/all.min.js"></script>
+  </body>
 </html>
