009b3f6ca79a9b9abb0b5df5cba66c53cb56a66f
ZaidAltamari <93926834+ZaidAltamari@users.noreply.github.com>
2022-03-20 19:55:55 +0200

    login animation

--- bootstrap-js/login.js
+++ bootstrap-js/login.js
@@ -1,4 +1,28 @@
 // jquery code to hide h1
 $(document).ready(function () {
-  $("h1").hide();
+  $(".right").animate({
+    top: '18px',  
+    AnimationEffect: 'linear',
+  }, 700);
+  $(".right").animate(
+    {
+      top: "8px",
+      AnimationEffect: 'linear',
+    },
+    100
+  );
+  $(".right").animate(
+    {
+      top: "18px",
+      AnimationEffect: "linear",
+    },
+    300
+  );
+  $(".left img ").animate({
+    transform: '',
+  }, 1000
+  );
+
+
+
 });
