4e3eb86ad7462692b2a9805185cb135f232189db
radwan susan <radwansusan90@gmail.com>
2022-03-23 16:16:01 +0300

    Login Animation Fix
    & indentation changes

--- login.php
+++ login.php
@@ -1,49 +1,45 @@
 <?php
-
 //include "loginphp.php";
 $servername = $_SERVER['SERVER_NAME'];
 $username = "username";
 $password = "password";
 
 // Create connection
-$conn = new mysqli($servername, "root","" ,"triibe");
+$conn = new mysqli($servername, "root", "", "triibe");
 
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}else{
-// echo "Connected successfully" . "<br>";
+	die("Connection failed: " . $conn->connect_error);
+}
+else {
+	// echo "Connected successfully" . "<br>";
 }
 
 session_start();
 
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	// username and password sent from form
+	$myusername = mysqli_real_escape_string($conn, $_POST['uname']);
+	$mypassword = mysqli_real_escape_string($conn, $_POST['password']);
 
-      $myusername = mysqli_real_escape_string($conn,$_POST['uname']);
-      $mypassword = mysqli_real_escape_string($conn,$_POST['password']);
+	$sql = "SELECT * FROM student WHERE std_id = '$myusername' and std_pass = '$mypassword'";
+	$result = mysqli_query($conn, $sql);
+	$row = mysqli_fetch_assoc($result);
+	//$active = $row['active'];
+	$count = mysqli_num_rows($result);
 
-      $sql = "SELECT * FROM student WHERE std_id = '$myusername' and std_pass = '$mypassword'";
-      $result = mysqli_query($conn,$sql);
-      $row = mysqli_fetch_assoc($result);
-      //$active = $row['active'];
-
-      $count = mysqli_num_rows($result);
-
-      // If result matched $myusername and $mypassword, table row must be 1 row
-
-      if($count == 1) {
-         //session_register("myusername");
-         $_SESSION['login_user'] = $myusername;
-         echo "<p>looged in</p>";
-        // header("location: homepage.php");
-      }else {
-         echo"Your Login Name or Password is invalid";
-      }
-    }
-
+	// If result matched $myusername and $mypassword, table row must be 1 row
+	if ($count == 1) {
+		//session_register("myusername");
+		$_SESSION['login_user'] = $myusername;
+		echo "<p>looged in</p>";
+		// header("location: homepage.php");
+	}
+	else {
+		echo "Your Login Name or Password is invalid";
+	}
+}
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -67,7 +63,7 @@ session_start();
       </div>
       <div class="right animate__animated animate__backInDown">
         <form action="" method="POST">
-          <input type="text" name="uname" placeholder="Email Or student number" />
+          <input type="text" name="uname" placeholder="Student number or Email" />
           <input type="password" name="password" placeholder="Password" />
           <button type="submit" href="" class="loginBtn">Log In</button>
           <a href="" class="forget">Forgotten password?</a>
