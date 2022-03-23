376c8468fc3c8d658b732ed15f4f7e4939afc464
radwan susan <radwansusan90@gmail.com>
2022-03-23 15:35:21 +0300

    Title Update

--- login.php
+++ login.php
@@ -50,7 +50,7 @@ session_start();
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>Triibe</title>
     <link rel="stylesheet" href="bootstrap-css/bootstrap.min.css" />
     <link rel="stylesheet" href="bootstrap-css/all.min.css" />
     <link rel="stylesheet" href="node_modules/animate.css/animate.css">
