c85537568d1a7d549ba3510be4675318b0da17ed
radwan susan <radwansusan90@gmail.com>
2022-03-25 17:43:26 +0300

    Login-Page Changes
    Login Page Alert If the User Enter Invalid Info
    File Management
    Added alertify.js Library

--- connection.php
+++ connection.php
@@ -2,10 +2,8 @@
 $servername = $_SERVER['SERVER_NAME'];
 $username = "username";
 $password = "password";
-
 // Create connection
 $conn = new mysqli($servername, "root", "", "triibe");
-
 // Check connection
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
@@ -13,3 +11,4 @@ if ($conn->connect_error) {
 else {
 	// echo "Connected successfully" . "<br>";
 }
+?>
--- login.php
+++ login.php
@@ -1,31 +1,6 @@
 <?php
 include "connection.php";
-
-
 session_start();
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	// username and password sent from form
-	$myusername = mysqli_real_escape_string($conn, $_POST['uname']);
-	$mypassword = mysqli_real_escape_string($conn, $_POST['password']);
-
-	$sql = "SELECT * FROM student WHERE std_id = '$myusername' and std_pass = '$mypassword'";
-	$result = mysqli_query($conn, $sql);
-	$row = mysqli_fetch_assoc($result);
-	//$active = $row['active'];
-	$count = mysqli_num_rows($result);
-
-	// If result matched $myusername and $mypassword, table row must be 1 row
-	if ($count == 1) {
-		//session_register("myusername");
-		$_SESSION['login_user'] = $myusername;
-		echo "<p>looged in</p>" .  $row['account_id'];
-		// header("location: homepage.php");
-	}
-	else {
-		echo "Your Login Name or Password is invalid";
-	}
-}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -37,19 +12,48 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <link rel="stylesheet" href="bootstrap-css/bootstrap.min.css" />
     <link rel="stylesheet" href="bootstrap-css/all.min.css" />
     <link rel="stylesheet" href="node_modules/animate.css/animate.css">
+    <link rel="stylesheet" href="node_modules/alertifyjs/build/css/alertify.min.css" />
+    <link rel="stylesheet" href="node_modules/alertifyjs/build/css/themes/default.min.css" />
     <link rel="stylesheet" href="bootstrap-css/login-style.css" />
+    <script src="node_modules/alertifyjs/build/alertify.min.js"></script>
+    <script type="text/javascript">
+      function alert(message){
+        alertify.defaults.glossary.title = 'My Title';
+        alertify.alert("Triibe",message);
+      }
+    </script>
   </head>
   <body>
     <div class="container1">
       <div class="left">
         <div class="box">
-          <img class="svg-img animate__animated animate__rollIn" src="Design/Image/logo.svg" style="transform: rotate(20deg);" alt="Triibe"/>
+          <img class="svg-img animate__animated animate__rollIn" src="Design/Image/logo.svg" alt="Triibe"/>
           <h1>Triibe</h1>
         </div>
         <p>
           <span class="typed"></span>
         </p>
       </div>
+      <?php if ($_SERVER["REQUEST_METHOD"] == "POST") {
+          // username and password sent from form
+          $myusername = mysqli_real_escape_string($conn, $_POST["uname"]);
+          $mypassword = mysqli_real_escape_string($conn, $_POST["password"]);
+
+          $sql = "SELECT * FROM student WHERE std_id = '$myusername' and std_pass = '$mypassword'";
+          $result = mysqli_query($conn, $sql);
+          $row = mysqli_fetch_assoc($result);
+          //$active = $row['active'];
+          $count = mysqli_num_rows($result);
+          // If result matched $myusername and $mypassword, table row must be 1 row
+          if ($count == 1) {
+              //session_register("myusername");
+              $_SESSION["login_user"] = $myusername;
+              echo "<p>looged in</p>" . $row["account_id"];
+              // header("location: homepage.php");
+          } else {
+              echo '<script type="text/javascript">alert("Invalid Information, Try again!");</script>';
+          }
+      } ?>
       <div class="right animate__animated animate__backInDown">
         <form action="" method="POST">
           <input type="text" name="uname" placeholder="Student number or Email" />
