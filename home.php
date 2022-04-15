04577293647436253b516da8341f73bcba57bd42
radwan susan <radwansusan90@gmail.com>
2022-04-15 15:46:03 +0300

    شغل ورا التعمري 2

--- home.php
+++ home.php
@@ -22,7 +22,7 @@
         <div class="left-top-card">
           <div class="card-name-photo">
             <img class="card-user-photo" src="Design/Image/home-images/images/radwan.jpg" alt="">
-            <div class="card-name">omar thaer</div>
+            <div class="card-name">Radwan Susan</div>
           </div>
           <div class="card-inside-top">
             <img src="Design/Image/home-images/images/ball2.svg" alt="">
