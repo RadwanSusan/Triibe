945e4b823a9dfb9c56db39d1222322c4bf7b6d2a
OmarThaerQ <othaer60@gmail.com>
2022-05-06 04:32:22 +0300

    still working in chat sys

--- bootstrap-js/chat.js
+++ bootstrap-js/chat.js
@@ -1,3 +1,5 @@
+$(document).ready(function() {
+});
 (function () {
 	var chat = {
 		messageToSend: "",
--- chat.php
+++ chat.php
@@ -19,54 +19,66 @@ session_start();
       <i class="fa fa-search"></i>
     </div>
     <ul class="list">
-<?php
-$sql = "SELECT * FROM messages WHERE from_user = '".$_SESSION["std_id"]."' OR to_user = '".$_SESSION["std_id"]."' ";
-$result = mysqli_query($conn, $sql);
-while($row = mysqli_fetch_array($result)){
-  if($row["from_user"] == $_SESSION["std_id"]){
+      <?php
+      $sql = "SELECT distinct * FROM messages where to_user = '".$_SESSION["std_id"]."'"; 
+      $result = mysqli_query($conn, $sql);
+      if (mysqli_num_rows($result) > 0) {
+      while($row = mysqli_fetch_array($result)){
+        $sql2 = "SELECT * FROM student where std_id = '".$row["from_user"]."'";
+        $result2 = mysqli_query($conn, $sql2);
+        $row2 = mysqli_fetch_array($result2);
+        $img_id = $row2["img_id"];
+        $status = $row2["status"];
+        if($status == "0"){
+          $status = "<i class='fa fa-circle offline'></i> offline";
+        }else{
+          $status = "<i class='fa fa-circle online'></i> online";
+        }
+        $sql3 = "SELECT * FROM img where img_id = '".$img_id."'";
+        $result3 = mysqli_query($conn, $sql3);
+        $row3 = mysqli_fetch_array($result3);
+        $img_name = $row3["img_name"];
+        echo "<li class='clearfix'>
+        <img src='".$img_name."' alt='avatar'/>
+        <div class='about'>
+          <div class='name'>".$row2["std_fname"]."".$row2["std_lname"]." </div>
+          <div class='status'>
+            ".$status."
+          </div>
+        </div>
+      </li>";
+      }
+    }else{
+      $sql = "SELECT distinct * FROM messages where from_user = '".$_SESSION["std_id"]."'"; 
+      $result = mysqli_query($conn, $sql);
+      while($row = mysqli_fetch_array($result)){
+        $sql2 = "SELECT * FROM student where std_id = '".$row["to_user"]."'";
+        $result2 = mysqli_query($conn, $sql2);
+        $row2 = mysqli_fetch_array($result2);
+        $img_id = $row2["img_id"];
+        $status = $row2["status"];
+        if($status == "0"){
+          $status = "<i class='fa fa-circle offline'></i> offline";
+        }else{
+          $status = "<i class='fa fa-circle online'></i> online";
+        }
+        $sql3 = "SELECT * FROM img where img_id = '".$img_id."'";
+        $result3 = mysqli_query($conn, $sql3);
+        $row3 = mysqli_fetch_array($result3);
+        $img_name = $row3["img_name"];
+        echo "<li class='clearfix'>
+        <img src='".$img_name."' alt='avatar'/>
+        <div class='about'>
+          <div class='name'>".$row2["std_fname"]." ".$row2["std_lname"]." </div>
+          <div class='status'>
+            ".$status."
+          </div>
+        </div>
+      </li>";
+      }
+    }
 
-  
-  $sql2 = "SELECT * FROM student WHERE std_id = '".$row["from_user"]."'";
-  $result2 = mysqli_query($conn, $sql2);
-  $row2 = mysqli_fetch_array($result2);
-  $img_id = $row2["img_id"];
-  $sql34= "SELECT * FROM img WHERE img_id = '".$img_id."'";
-  $result34 = mysqli_query($conn, $sql34);
-  $row34 = mysqli_fetch_array($result34);
-  $imgname = $row34["img_name"];
-  echo "<li class='clearfix'>";
-  echo "<div class='img'>";
-  echo "<img src='".$imgname."' alt='' />";
-  echo "</div>";
-  echo "<div class='about'>";
-  echo "<div class='name'>".$row2["std_fname"]."</div>";
-  echo "<div class='status'>".$row["message"]."</div>";
-  echo "</div>";
-  echo "</li>";
-  }else{
-  $sql3 = "SELECT * FROM student WHERE std_id = '".$row["to_user"]."'";
-  $result3 = mysqli_query($conn, $sql3);
-  $row3 = mysqli_fetch_array($result3);
-  $img_id = $row3["img_id"];
-  $sql34= "SELECT * FROM img WHERE img_id = '".$img_id."'";
-  $result34 = mysqli_query($conn, $sql34);
-  $row34 = mysqli_fetch_array($result34);
-  $imgname = $row34["img_name"];
-  
-    echo "<li class='clearfix'>";
-    echo "<div class='img'>";
-    echo "<img src='".$imgname."' alt='' />";
-    echo "</div>";
-    echo "<div class='about'>";
-    echo "<div class='name'>".$row3["std_fname"]."</div>";
-    echo "<div class='status'>".$row["message"]."</div>";
-    echo "</div>";
-    echo "</li>";
-}
-}
-?>
-     
-
+      ?>
     </ul>
   </div>
 
@@ -82,60 +94,75 @@ while($row = mysqli_fetch_array($result)){
     </div> <!-- end chat-header -->
 
     <div class="chat-history">
-      <ul>
-        <li class="clearfix">
-          <div class="message-data align-right">
-            <span class="message-data-time">10:10 AM, Today</span> &nbsp; &nbsp;
-            <span class="message-data-name">Olia</span> <i class="fa fa-circle me"></i>
+     <ul>
+     <?php
+$sql = "SELECT * FROM messages WHERE from_user = '".$_SESSION["std_id"]."' OR to_user = '".$_SESSION["std_id"]."' ORDER BY time DESC";
+$result = mysqli_query($conn, $sql);
+while($row = mysqli_fetch_array($result)){
+                            $now = new DateTime();
+                            $post = new DateTime($row["time"]);
+                            $diff = $now->diff($post);
+                            $diff->format("%a");
+                            $diffday = $diff->format("%a");
+                            $diffhour = $diff->format("%h");
+                            $diffminute = $diff->format("%i");
+                            $diffsecond = $diff->format("%s");
+                            $diffdaystr = (string)$diffday;
+                            $diffhourstr = (string)$diffhour;
+                            $diffminutestr = (string)$diffminute;
+                            $diffsecondstr = (string)$diffsecond;
+                            $difftime = $diffsecondstr . "second ago";
+                            if ($diffdaystr == "0"){
+                                if ($diffhourstr == "0"){
+                                    if ($diffminutestr == "0"){
+                                        $difftime = $diffsecondstr . "s ago";
+                                    }
+                                    else{
+                                        $difftime = $diffminutestr . "m ago";
+                                    }
+                                }
+                                else{
+                                    $difftime = $diffhourstr . "h ago";
+                                }
+                            }
+                            else{
+                                $difftime = $diffdaystr . "d ago";
+                            }
+  if($row["from_user"] == $_SESSION["std_id"]){
+  $sql2 = "SELECT * FROM student WHERE std_id = '".$row["from_user"]."'";
+  $result2 = mysqli_query($conn, $sql2);
+  $row2 = mysqli_fetch_array($result2);
+
+  echo "<li class='clearfix'>
+          <div class='message-data align-right'>
+            <span class='message-data-time'>".$difftime."</span> &nbsp; &nbsp;
+            <span class='message-data-name'>".$row2["std_fname"]."</span> <i class='fa fa-circle me'></i>
 
           </div>
-          <div class="message other-message float-right">
-            Hi Vincent, how are you? How is the project coming along?
+          <div class='message other-message float-right'>
+            ".$row["message"]."
           </div>
-        </li>
+        </li>";
+  }
+  else{
+  $sql3 = "SELECT * FROM student WHERE std_id = '".$row["from_user"]."'";
+  $result3 = mysqli_query($conn, $sql3);
+  $row3 = mysqli_fetch_array($result3);  
 
-        <li>
-          <div class="message-data">
-            <span class="message-data-name"><i class="fa fa-circle online"></i> Vincent</span>
-            <span class="message-data-time">10:12 AM, Today</span>
-          </div>
-          <div class="message my-message">
-            Are we meeting today? Project has been already finished and I have results to show you.
-          </div>
-        </li>
+    echo "<li>
+    <div class='message-data'>
+      <span class='message-data-name'><i class='fa fa-circle online'></i>".$row3["std_fname"]."</span>
+      <span class='message-data-time'>".$difftime."</span>
+    </div>
+    <div class='message my-message'>
+      ".$row["message"]."
+    </div>
+  </li>";
+}
+}
+?>
 
-        <li class="clearfix">
-          <div class="message-data align-right">
-            <span class="message-data-time">10:14 AM, Today</span> &nbsp; &nbsp;
-            <span class="message-data-name">Olia</span> <i class="fa fa-circle me"></i>
-
-          </div>
-          <div class="message other-message float-right">
-            Well I am not sure. The rest of the team is not here yet. Maybe in an hour or so? Have you faced any problems at the last phase of the project?
-          </div>
-        </li>
-
-        <li>
-          <div class="message-data">
-            <span class="message-data-name"><i class="fa fa-circle online"></i> Vincent</span>
-            <span class="message-data-time">10:20 AM, Today</span>
-          </div>
-          <div class="message my-message">
-            Actually everything was fine. I'm very excited to show this to our team.
-          </div>
-        </li>
-
-        <li>
-          <div class="message-data">
-            <span class="message-data-name"><i class="fa fa-circle online"></i> Vincent</span>
-            <span class="message-data-time">10:31 AM, Today</span>
-          </div>
-          <i class="fa fa-circle online"></i>
-          <i class="fa fa-circle online" style="color: #AED2A6"></i>
-          <i class="fa fa-circle online" style="color:#DAE9DA"></i>
-        </li>
-
-      </ul>
+     </ul>
 
     </div> <!-- end chat-history -->
 
