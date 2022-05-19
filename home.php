a6e04eb00ffcb7cac258cef5a2e1fdf69e0b493f
radwan susan <radwansusan90@gmail.com>
2022-05-19 20:08:22 +0300

    New Left Sidebar Design Change

--- home.php
+++ home.php
@@ -361,13 +361,13 @@ include_once "backBone.php";
     <div class="nav-right">
       <ul>
         <a class="list2" href="list.php">
-        <li class="more-list">
-          <img class="SettingsIcon-Light" src="Design/Image/home-images/images/more-list.svg" alt="settingIcon" />
-        </li>
-        <li>
-          </a>
-          <img class="SettingsIcon-Light" src="Design/Image/home-images/images/Settings-icon.svg" alt="settingIcon" />
-          <img class="SettingsIcon-Dark" src="Design/Image/home-images/images/Settings-icon2.svg" alt="settingIcon" />
+          <li class="more-list">
+            <img class="SettingsIcon-Light" src="Design/Image/home-images/images/more-list.svg" alt="settingIcon" />
+          </li>
+          <li>
+        </a>
+        <img class="SettingsIcon-Light" src="Design/Image/home-images/images/Settings-icon.svg" alt="settingIcon" />
+        <img class="SettingsIcon-Dark" src="Design/Image/home-images/images/Settings-icon2.svg" alt="settingIcon" />
         </li>
         <li>
           <img class="mapIcon-Light" src="Design/Image/home-images/images/mapIcon.svg" alt="mapIcon" />
@@ -398,59 +398,64 @@ include_once "backBone.php";
   </nav>
   <div class="container1">
     <div class="left-sidebar">
-      <div class="group-list">
+      <div class="group-page2">
         <p>Collage </p>
         <?php
         $sqlc = "SELECT Coll_Name,Coll_No FROM colleges WHERE Coll_NO = (SELECT Coll_Major_No FROM majors WHERE ID = (SELECT Std_Major_No FROM students WHERE std_No = '" . $_SESSION["std_id"] . "'))";
         $resultc = mysqli_query($conn, $sqlc);
         $rowc = mysqli_fetch_assoc($resultc);
-        echo "<a href='#' class='group-list-item' data-form_id="."0".$rowc["Coll_No"].">" . $rowc["Coll_Name"] . "</a>";
+        echo "<a href='#' class='group-list-item' data-form_id=" . "0" . $rowc["Coll_No"] . ">" . $rowc["Coll_Name"] . "</a>";
         ?>
+      </div>
+      <div class="group-page2">
         <p>major</p>
         <?php
         $sqlm = "SELECT Major_Name,Major_No FROM majors WHERE ID = (SELECT Std_Major_No FROM students WHERE std_No = '" . $_SESSION["std_id"] . "')";
         $resultm = mysqli_query($conn, $sqlm);
         $rowm = mysqli_fetch_assoc($resultm);
-        echo "<a href='#' class='group-list-item' data-form_id="."1".$rowm["Major_No"].">" . $rowm["Major_Name"] . "</a>";        ?>
+        echo "<a href='#' class='group-list-item' data-form_id=" . "1" . $rowm["Major_No"] . ">" . $rowm["Major_Name"] . "</a>";
+        ?>
+      </div>
+      <div class="group-page2">
         <p> Subjects </p>
         <?php
         $sqlsub = "SELECT Course_Name,ID FROM courses WHERE ID IN (SELECT crs_id FROM subjects where id IN (SELECT sub_id FROM std_crs_temp WHERE std_id IN (SELECT id FROM students WHERE Std_No =" . $_SESSION["std_id"] . ")))";
         $resultsub = mysqli_query($conn, $sqlsub);
         if (mysqli_num_rows($resultsub) > 0) {
           while ($rowsub = mysqli_fetch_assoc($resultsub)) {
-            echo "<a href='#' class='group-list-item' data-form_id="."2".$rowsub["ID"].">" . $rowsub["Course_Name"] . "</a>";
+            echo "<a href='#' class='group-list-item' data-form_id=" . "2" . $rowsub["ID"] . ">" . $rowsub["Course_Name"] . "</a>";
           }
         }
         ?>
-        <div class="group-page">
-          <p>Friends</p>
-          <?php $sql = "SELECT * FROM friends WHERE user_id = '" . $_SESSION["std_id"] . "'";
-          $result = mysqli_query($conn, $sql);
-          if (mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-              $sql1 = "SELECT * FROM student WHERE std_id = '" . $row["friend_id"] . "'";
-              $result1 = mysqli_query($conn, $sql1);
-              if (mysqli_num_rows($result1) > 0) {
-                while ($row1 = mysqli_fetch_assoc($result1)) {
-                  $imgid = $row1["img_id"];
-                  $sqlimg = "SELECT * FROM img WHERE img_id = '$imgid'";
-                  $resultimg = mysqli_query($conn, $sqlimg);
-                  $rowimg = mysqli_fetch_assoc($resultimg);
-                  if (isset($rowimg["img_name"])) {
-                    $imgname = $rowimg["img_name"];
+      </div>
+      <div class="group-page">
+        <p>Friends</p>
+        <?php $sql = "SELECT * FROM friends WHERE user_id = '" . $_SESSION["std_id"] . "'";
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) {
+          while ($row = mysqli_fetch_assoc($result)) {
+            $sql1 = "SELECT * FROM student WHERE std_id = '" . $row["friend_id"] . "'";
+            $result1 = mysqli_query($conn, $sql1);
+            if (mysqli_num_rows($result1) > 0) {
+              while ($row1 = mysqli_fetch_assoc($result1)) {
+                $imgid = $row1["img_id"];
+                $sqlimg = "SELECT * FROM img WHERE img_id = '$imgid'";
+                $resultimg = mysqli_query($conn, $sqlimg);
+                $rowimg = mysqli_fetch_assoc($resultimg);
+                if (isset($rowimg["img_name"])) {
+                  $imgname = $rowimg["img_name"];
+                } else {
+                  if ($row1["gender"] == 1) {
+                    $imgname = "Design\Image\LogoPic0.jpg";
                   } else {
-                    if ($row1["gender"] == 1) {
-                      $imgname = "Design\Image\LogoPic0.jpg";
-                    } else {
-                      $imgname = "Design\Image\LogoPic1.jpg";
-                    }
+                    $imgname = "Design\Image\LogoPic1.jpg";
                   }
-                  echo "<a href='friendpage.php?account_id=" . $row1["account_id"] . "'><img src='$imgname'/>" . $row1["std_fname"] . " " . $row1["std_lname"] . "</a>";
                 }
+                echo "<a href='friendpage.php?account_id=" . $row1["account_id"] . "'><img src='$imgname'/>" . $row1["std_fname"] . " " . $row1["std_lname"] . "</a>";
               }
             }
-          } ?>
-        </div>
+          }
+        } ?>
       </div>
     </div>
     <div class="main-content animate__animated animate__fadeIn animate__slower">
