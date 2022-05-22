3e9681a0fff663e4cd4c08beb6796bbbd367dbc8
OmarThaerQ <othaer60@gmail.com>
2022-05-22 21:14:27 +0300

    Update friends_request.sql

--- db/friends_request.sql
+++ db/friends_request.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 20, 2022 at 09:12 PM
+-- Generation Time: May 22, 2022 at 08:14 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.5
 
@@ -24,13 +24,14 @@ SET time_zone = "+00:00";
 CREATE TABLE `friends_request` (
   `sender` bigint(12) NOT NULL,
   `receiver` bigint(12) NOT NULL,
-  `status` int(1) NOT NULL DEFAULT 0
+  `status` int(1) NOT NULL DEFAULT 0,
+  `date` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `friends_request`
 --
 
-INSERT INTO `friends_request` (`sender`, `receiver`, `status`) VALUES
-(120180612100, 120201408061, 0);
+INSERT INTO `friends_request` (`sender`, `receiver`, `status`, `date`) VALUES
+(120180612999, 120180612100, 1, '2022-05-22 19:36:02');
 COMMIT;
