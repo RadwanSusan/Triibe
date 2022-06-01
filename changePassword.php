5df5c913eb5461d4abc2e16cd01a012eb7c521c1
radwan susan <radwansusan90@gmail.com>
2022-06-01 23:46:40 +0300

    Update changePassword.php

--- changePassword.php
+++ changePassword.php
@@ -2,10 +2,13 @@
 <html lang="en">
 
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Triibe Change Password</title>
+   <meta charset="UTF-8" />
+   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <link rel="icon" href="Design/Image/whiteLogo.svg" type="image/x-icon" />
+   <title>Triibe home</title>
+   <link rel="stylesheet" href="bootstrap-css/bootstrap.min.css" />
+   <link rel="stylesheet" href="bootstrap-css/all.min.css" />
    <link rel="stylesheet" href="bootstrap-css/changePass.css">
 </head>
 
@@ -23,10 +26,12 @@
                <button class="unmask" type="button"></button>
             <div id="strong"><span></span></div>
             <div id="valid"></div>
-            <small>Must be 6+ characters long and contain at least 1 upper case letter, 1 number, 1 special character</small>
             </p>
+            <button type="submit" id="submit" class="btn btn-primary">Change Password</button>
          </form>
    </div>
+   <script src="bootstrap-js/bootstrap.bundle.min.js"></script>
+   <script src="bootstrap-js/all.min.js"></script>
    <script src="node_modules/jquery/dist/jquery.min.js"></script>
    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
    <script src="bootstrap-js/changePass.js"></script>
