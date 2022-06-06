e4fd06d9ad6ab0c55de34a6d7edcd8fcd10042b5
radwan susan <radwansusan90@gmail.com>
2022-06-06 23:09:17 +0300

    Market Update

--- market.php
+++ market.php
@@ -8,10 +8,11 @@ $badwords = ["fuck", "shit", "bitch", "asshole", "dick", "pussy", "كس", "كس 
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="icon" href="Design/Image/whiteLogo.svg" type="image/x-icon" />
+  <title>Triibe Market</title>
   <link rel="stylesheet" href="bootstrap-css/bootstrap.min.css" />
   <link rel="stylesheet" href="bootstrap-css/all.min.css" />
   <link rel="stylesheet" href="node_modules/animate.css/animate.css" />
