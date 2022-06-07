a78c2bbd2fbb2c6f5e981e769111d38c399414fa
OmarThaerQ <othaer60@gmail.com>
2022-06-07 21:07:48 +0300

    ss

--- market.php
+++ market.php
@@ -163,7 +163,7 @@ $badwords = ["fuck", "shit", "bitch", "asshole", "dick", "pussy", "كس", "كس 
     <div class="top-card">
       <div class="left-top-card">
         <div class="card-name-photo">
-          <img class="card-user-photo" src="<?php echo $_SESSION["img_name"] ?>" alt="">
+          <img class="card-user-photo" src="<?php echo $_SESSION["personalProfile"] ?>" alt="">
           <div class="card-name"><?php echo $_SESSION["std_fname"] . " " . $_SESSION["std_lname"] ?></div>
         </div>
         <div class="card-inside-top">
